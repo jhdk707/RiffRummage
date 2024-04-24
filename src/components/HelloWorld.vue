954780bb6d7c823ed1c98f92ffcec9869a6584d4
Doomtower <jessehudak707@outlook.com>
2024-04-24 16:51:16 -0700

    changed name to Riff Rummager, added content to about, created searchview and resultsview

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,9 +11,7 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      Looking for new music or info on your favorite heavy band? You've come to the right place.
     </h3>
   </div>
 </template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>
+      Riff Rummager is a fun project based on some early ideas I had in my coding bootcamp. I now
+      have a much larger project and expansion of the idea on another app that I am working on,
+      however I wanted to keep working on this as a side project. I used to spend HOURS every night
+      digging through metalarchives.com, looking for new music and I loved it. Once I found this
+      API, I knew that I had to make some kind of project out it, so here we are.
+    </h1>
   </div>
 </template>
 
